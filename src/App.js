6252d1cde710cf488550036988e8c55c58bab727
richard-dinh <richarddinh@gmail.com>
2020-03-18 15:47:24 -0700

    made App a smart react component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Navbar from './components/Navbar'
 import Searchbar from './components/Searchbar'
 import Card from './components/Card'
-function App() {
-  return (
-    <>
-      <Navbar />
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
-            <Searchbar />
-          </div>
-          <div className="col-12">
-            <div className="container">
-              <div className="row">
-                {/* cards go here */}
-                <Card />
-                <Card />
-                <Card />
+
+class App extends Component {
+
+  render(){
+
+    return (
+      <>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Searchbar />
+            </div>
+            <div className="col-12">
+              <div className="container">
+                <div className="row">
+                  {/* cards go here */}
+                  <Card />
+                  <Card />
+                  <Card />
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
+
 
 export default App
